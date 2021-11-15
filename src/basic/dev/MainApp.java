37111138acee1c4aa1279935298c5649d09131d8
minhhieu08082003 <tranminhhieu08082003@gmail.com>
2021-11-15 12:08:11 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// Thông tin một sinh viên
+		String ten;
+		String maSinhVien;
+		String chungMinhNhanDan;
+		String tenLop;
+		int diemTrungBinh;
+		
+		//Thông tin công dân
+		String name;
+		String diaChi;
+		String soDienThoai;
+		String heSoLuong;
+		
+		//Thông tin một chiếc xe
+		String tenXe;
+		String hang;
+		int gia;
+		String mauSon;
+		
+			
+	}
+
+}
